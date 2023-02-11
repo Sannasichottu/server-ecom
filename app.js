b67a386860d213a53a5be1c8ddbf39656032e5ce
Sannasi <sannasichottu@gmail.com>
2023-02-11 21:50:55 +0530

    product count

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const errorMiddleware = require("./middlewares/error")
 const cookieParser = require('cookie-parser')
+const cors = require('cors');
 
+
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct,
 const router = express.Router();
 const { isAuthenticateUser, authorizeRoles } = require('../middlewares/authenticate');
 
-router.route("/products").get(isAuthenticateUser, getProducts);
+router.route("/products").get( getProducts);
 router.route("/product/:id").get(getSingleProduct);
 router.route("/product/:id").put(updateProduct);
 router.route("/product/:id").delete(deleteProduct);
