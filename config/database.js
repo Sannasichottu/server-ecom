abb62ae2596e16434c13be48b073d8a402c2baf1
Sannasi <sannasichottu@gmail.com>
2023-02-13 13:30:56 +0530

    user authenticate cmplt

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const connectDatabase = () => {
-    mongoose.connect(process.env.ATLAS_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(con=> {
-        console.log("Database connect successfully")
+  mongoose
+    .connect(process.env.ATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-}
+    .then((con) => {
+      console.log("Database connect successfully");
+    });
+};
 
 module.exports = connectDatabase;
