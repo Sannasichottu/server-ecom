959e486fa007f00af45d0d64e10891ec6f4ddfd1
Sannasi <sannasichottu@gmail.com>
2023-02-25 16:12:18 +0530

    admin product created

--- controllers/productController.js
+++ controllers/productController.js
@@ -32,6 +32,18 @@ exports.getProducts = catchAsyncError(async (req, res, next) => {
 
 // Create Product - /api/v1/product/new
 exports.newProduct = catchAsyncError(async (req, res, next) => {
+  let images = [];
+
+  if (req.files.length > 0) {
+    req.files.forEach((file) => {
+      //let url = `${process.env.BACKEND_URL}/uploads/product/${req.file.originalname}`;
+      let url = `${process.env.BACKEND_URL}/uploads/product/${file.originalname}`;
+      images.push({ image: url });
+    });
+  }
+
+  req.body.images = images;
+
   req.body.user = req.user.id;
   const product = await Product.create(req.body);
   res.status(201).json({
@@ -182,5 +194,14 @@ exports.deleteReiew = catchAsyncError(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
+  });
+});
+
+//get admin products - api/v1/admin/products
+exports.getAdminProducts = catchAsyncError(async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).send({
+    success: true,
+    products,
   });
 });
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,36 @@
-const express = require('express');
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createReview, getReviews, deleteReiew } = require('../controllers/productController');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const {
+  getProducts,
+  newProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  createReview,
+  getReviews,
+  deleteReiew,
+  getAdminProducts,
+} = require("../controllers/productController");
 const router = express.Router();
-const { isAuthenticateUser, authorizeRoles } = require('../middlewares/authenticate');
+const {
+  isAuthenticateUser,
+  authorizeRoles,
+} = require("../middlewares/authenticate");
 
-router.route("/products").get( getProducts);
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      //cb- callback
+      cb(null, path.join(__dirname, "..", "uploads/product"));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+
+router.route("/products").get(getProducts);
 router.route("/product/:id").get(getSingleProduct);
 router.route("/product/:id").put(updateProduct);
 router.route("/product/:id").delete(deleteProduct);
@@ -14,6 +41,16 @@ router.route("/reviews").get(getReviews);
 router.route("/review").delete(deleteReiew);
 
 //Admin routes
-router.route("/admin/product/new").post(isAuthenticateUser, authorizeRoles('admin'), newProduct);
+router
+  .route("/admin/product/new")
+  .post(
+    isAuthenticateUser,
+    authorizeRoles("admin"),
+    upload.array("images"),
+    newProduct
+  );
+router
+  .route("/admin/products")
+  .get(isAuthenticateUser, authorizeRoles("admin"), getAdminProducts);
 
 module.exports = router;
