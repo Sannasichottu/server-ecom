8cbfde8f2d0324ec18a62bca5c5455481d0c0283
Sannasi <sannasichottu@gmail.com>
2023-02-27 23:06:10 +0530

    admin order crud

--- routes/order.js
+++ routes/order.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const { newOrder, getSingleOrder, myOrders, orders, updateOrder, deleteOrder } = require('../controllers/orderController');
-const { isAuthenticateUser, authorizeRoles } = require('../middlewares/authenticate');
+const express = require("express");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  orders,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
+const {
+  isAuthenticateUser,
+  authorizeRoles,
+} = require("../middlewares/authenticate");
 const router = express.Router();
 
-
-
-router.route('/order/new').post(isAuthenticateUser, newOrder);
-router.route('/order/:id').get(isAuthenticateUser, getSingleOrder);
-router.route('/myorders').get(isAuthenticateUser, myOrders);
-
+router.route("/order/new").post(isAuthenticateUser, newOrder);
+router.route("/order/:id").get(isAuthenticateUser, getSingleOrder);
+router.route("/myorders").get(isAuthenticateUser, myOrders);
 
 //Admin routes
-router.route("/orders").get(isAuthenticateUser, authorizeRoles('admin'), orders);
-router.route("/order/:id").put(isAuthenticateUser, authorizeRoles('admin'), updateOrder);
-router.route("/order/:id").delete(isAuthenticateUser, authorizeRoles('admin'), deleteOrder);
+router
+  .route("/admin/orders")
+  .get(isAuthenticateUser, authorizeRoles("admin"), orders);
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticateUser, authorizeRoles("admin"), updateOrder);
+router
+  .route("/admin/order/:id")
+  .delete(isAuthenticateUser, authorizeRoles("admin"), deleteOrder);
 
-module.exports =router;
+module.exports = router;
