05ec1d5ba83ae1ce6c5dc77e326a5aeb86ac7a1a
Sannasi <sannasichottu@gmail.com>
2023-03-03 15:03:43 +0530

    env update

--- controllers/productController.js
+++ controllers/productController.js
@@ -34,9 +34,15 @@ exports.getProducts = catchAsyncError(async (req, res, next) => {
 exports.newProduct = catchAsyncError(async (req, res, next) => {
   //uploading images
   let images = [];
+
+  let BASE_URL = process.env.BACKEND_URL;
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = `${req.protocol}://${req.get("host")}`;
+  }
+
   if (req.files.length > 0) {
     req.files.forEach((file) => {
-      let url = `${process.env.BACKEND_URL}/uploads/product/${file.originalname}`;
+      let url = `${BASE_URL}/uploads/product/${file.originalname}`;
       images.push({ image: url });
     });
   }
@@ -82,9 +88,14 @@ exports.updateProduct = catchAsyncError(async (req, res, next) => {
     images = product.images;
   }
 
+  let BASE_URL = process.env.BACKEND_URL;
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = `${req.protocol}://${req.get("host")}`;
+  }
+
   if (req.files.length > 0) {
     req.files.forEach((file) => {
-      let url = `${process.env.BACKEND_URL}/uploads/product/${file.originalname}`;
+      let url = `${BASE_URL}/uploads/product/${file.originalname}`;
       images.push({ image: url });
     });
   }
